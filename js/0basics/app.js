bdc5138484b98b1078db34f442a9ee1e425fbca5
Mansoor Nasir <mrmansoornasir@gmail.com>
2021-07-02 16:07:19 +0500

    js

---
+++ js/0basics/app.js
@@ -0,0 +1,58 @@
+// console.log('Message from NodeJS.')
+// console.log('You are very late. Abdul Baqi.')
+// console.log('This is another message.')
+
+// var f = 10;
+// var m = 5;
+
+// var c = f + m;
+
+// console.log(c)
+
+// alert('This is it')
+
+var data = [
+    {
+        name: "Shahid",
+        age: 21,
+        gpa: 3.2,
+        isStudent: true,
+        marks: [30, 40, 50, 60, 0],
+        subjects: {
+            cpp: 3.2,
+            java: 4.0,
+            python: 2.3,
+        },
+    },
+    {
+        name: "Shuja",
+        age: 24,
+        gpa: 3.3,
+        isStudent: false,
+        marks: [20, 60, 40, 60, 10],
+        subjects: {
+            cpp: 3.3,
+            java: 1.0,
+            python: 3.3,
+        },
+    },
+    {
+        name: "Kamran",
+        age: 24,
+        gpa: 3.5,
+        isStudent: true,
+        marks: [33, 50, 70, 20, 10],
+        subjects: {
+            cpp: 1.2,
+            java: 3.0,
+            python: 4.0,
+        },
+    },
+];
+
+// console.log(data.name);
+// console.log(data.age);
+// console.log(data.isStudent);
+// console.log(data.marks[5]);
+// console.log(data.subjects.python);
+// console.log(data.subjects["python"]);
